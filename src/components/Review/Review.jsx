22a60828d2d294a2e60ac9b53e38d959ccbe5be6
Beth Tracy <bntracy@gmail.com>
2024-02-23 20:40:47 -0600

    review page done

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -1,5 +1,30 @@
+import axios from 'axios';
+import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+
 function Review() {
-    return <></>;
+    const history = useHistory();
+    const feelingNum = useSelector(store => store.feelingNum);
+    const understandingNum = useSelector(store => store.understandingNum);
+    const supportNum = useSelector(store => store.supportNum);
+    const commentsText = useSelector(store => store.commentsText);
+
+    const handleClick = () => {
+        axios.post('/api/feedback', {feelingNum, understandingNum, supportNum, commentsText}).then(response => {
+            history.push('/thank-you');
+        }).catch(err => {
+            console.log('Error sending data to database', err);
+        });
+    }
+
+    return <>
+        <h1>Review Your Feedback</h1>
+        <p>Feelings: {feelingNum}</p>
+        <p>Understanding: {understandingNum}</p>
+        <p>Support: {supportNum}</p>
+        <p>Comments: {commentsText}</p>
+        <button data-testid="next" onClick={handleClick}>Submit</button>
+    </>;
 }
 
 export default Review;
